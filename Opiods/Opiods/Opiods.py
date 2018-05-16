a39be64e0c5bb2e83195275e004a1bd69d82d567
Thomas Petersen <tom.petersen22@gmail.com>
2018-05-16 09:48:45 -0700

    Making some progress on input side

--- Opiods/Opiods/Opiods.py
+++ Opiods/Opiods/Opiods.py
@@ -22,20 +22,24 @@ K.set_image_data_format('channels_last')
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 
+X = load_dataset()
 
-X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+print ("X shape: " + str(X.shape))
+print (X)
+
+#X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
 
 # Normalize image vectors
-X_train = X_train_orig/255.
-X_test = X_test_orig/255.
+##X_train = X_train_orig/255.
+#X_test = X_test_orig/255.
 
 # Reshape
-Y_train = Y_train_orig.T
-Y_test = Y_test_orig.T
+#Y_train = Y_train_orig.T
+#Y_test = Y_test_orig.T
 
-print ("number of training examples = " + str(X_train.shape[0]))
-print ("number of test examples = " + str(X_test.shape[0]))
-print ("X_train shape: " + str(X_train.shape))
-print ("Y_train shape: " + str(Y_train.shape))
-print ("X_test shape: " + str(X_test.shape))
-print ("Y_test shape: " + str(Y_test.shape))
+#print ("number of training examples = " + str(X_train.shape[0]))
+#print ("number of test examples = " + str(X_test.shape[0]))
+#print ("X_train shape: " + str(X_train.shape))
+#print ("Y_train shape: " + str(Y_train.shape))
+#print ("X_test shape: " + str(X_test.shape))
+#print ("Y_test shape: " + str(Y_test.shape))
--- Opiods/Opiods/kt_utils.py
+++ Opiods/Opiods/kt_utils.py
@@ -1,26 +1,63 @@
+# 
+# Utilities for data file loading and conversion
+#
+
 import keras.backend as K
 import math
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
+import os.path
 
 
 def mean_pred(y_true, y_pred):
     return K.mean(y_pred)
+#
+# Reads an input dataset and converts to np binary for faster futuer loads
+#
 
-def load_dataset():
-    train_dataset = h5py.File('datasets/train_happy.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+def load_dataset (
+    xfilename = r'..\..\Example Medical Database\df_x_withGeo',
+    yfilename = r'..\..\Example Medical Database\data_y'):
 
-    test_dataset = h5py.File('datasets/test_happy.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    # Check for the real Medical Records Database in binary form
+    xCsvFilename = xfilename + ".csv"
+    XNpFilename = xfilename + ".npy"
 
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    # Look for the binary version first
+    if not os.path.isfile(XNpFilename):
+        
+        # Load the csv file
+        print ("Loading input file : " + xCsvFilename)
+        try :
+            data_x = np.genfromtxt(xCsvFilename, skip_header=0, names=True, delimiter=',', max_rows=10000)
+            np.save(XNpFilename, data_x);
+            print ("Saved binary version of input file : " + xCsvFilename)
+        except:
+            print ("Unable to read : " + xCsvFilename)
+
+
+    else:
+        print ("Loading binary input file : " + XNpFilename)
+        try:
+            data_x = np.load(XNpFilename)
+        except:
+            print ("Unable to open : " + xCsvFilename)
+
+    return (data_x)
+
+    #train_dataset = h5py.File('datasets/train_happy.h5', "r")
+    #train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    #train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    #test_dataset = h5py.File('datasets/test_happy.h5', "r")
+    #test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    #test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    #classes = np.array(test_dataset["list_classes"][:]) # the list of classes
     
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    #train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    #test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
     
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    #return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
