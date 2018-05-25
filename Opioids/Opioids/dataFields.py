a4e80cb5ce68875afbd88a65a4f0687851a5dcca
Chloe OC <chloeo@stanford.edu>
2018-05-25 13:57:00 -0700

    updated column names

--- Opioids/Opioids/dataFields.py
+++ Opioids/Opioids/dataFields.py
@@ -1,131 +1,22 @@
-xusecols = (
+import csv
+from dataParser import *
+import os
 
-    # Second version of scalated file
-    "geoRegion1",
-    "geoRegion2",
-    "geoRegion3",
-    "geoRegion4",
-    "geoRegion5",
-    "state1",
-    "state4",
-    "state5",
-    "state6",
-    "state7",
-    "state8",
-    "state9",
-    "state11",
-    "state12",
-    "state13",
-    "state16",
-    "state17",
-    "state18",
-    "state19",
-    "state20",
-    "state22",
-    "state23",
-    "state24",
-    "state25",
-    "state26",
-    "state27",
-    "state28",
-    "state31",
-    "state32",
-    "state33",
-    "state34",
-    "state35",
-    "state36",
-    "state37",
-    "state38",
-    "state39",
-    "state41",
-    "state42",
-    "state43",
-    "state44",
-    "state46",
-    "state47",
-    "state48",
-    "state49",
-    "state52",
-    "state53",
-    "state54",
-    "state55",
-    "state56",
-    "state57",
-    "state58",
-    "state59",
-    "state61",
-    "state62",
-    "state63",
-    "state64",
-    "state65",
-    "state97",
-    "htn0",
-    "htn1",
-    "chf0",
-    "chf1",
-    "copd0",
-    "copd1",
-    "MI0",
-    "MI1",
-    "diab_u0",
-    "diab_u1",
-    "diab_c0",
-    "diab_c1",
-    "obesity0",
-    "obesity1",
-    "drug0",
-    "drug1",
-    "healthPlan0",
-    "healthPlan1",
-    "alcohol0",
-    "alcohol1",
-    "depres60",
-    "depres61",
-    "depres20",
-    "depres21",
-    "multi_fusion0",
-    "multi_fusion1",
-    "tbco0",
-    "tbco1",
-    "instr0",
-    "instr1",
-    "anx20",
-    "anx21",
-    "anx60",
-    "anx61",
-    "schiz20",
-    "schiz21",
-    "schiz60",
-    "schiz61",
-    "bpd20",
-    "bpd21",
-    "bpd60",
-    "bpd61",
-    "multi_lami0",
-    "multi_lami1",
-    "single_lami0",
-    "single_lami1",
-    "tbco_tx0",
-    "tbco_tx1",
-    "sex0",
-    "sex1",
-    "pvd0",
-    "pvd1",
-    "preopAntipsych0",
-    "preopAntipsych1",
-    "preopAntidep0",
-    "preopAntidep1",
-    "preopBenzos0",
-    "preopBenzos1",
-    "age",
-    "year",
-    "charlson",
-    "preopDays",
-    "totalPreopSupply",
-    "longitude",
-    "latitude"
+absFilePath = os.path.abspath(__file__)
+fileDir = os.path.dirname(absFilePath)
+parentDir = os.path.dirname(os.path.dirname(os.path.dirname(fileDir)))
+dataFile1Path = os.path.join(parentDir, 'Medical Database/scaled.csv')
 
-)
+# Read the datasets
+Xpath= dataFile1Path
+
+with open(Xpath, "r") as f:
+    reader = csv.reader(f)
+    stringNames = next(reader)
+
+xusecols = (stringNames)
+
+print(xusecols)
             
 yusecols = (
     #"num",
@@ -142,3 +33,4 @@ yusecols = (
     #"cost_90d",
     #"dis_home"
 )
+
